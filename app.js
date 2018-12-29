49dc755b005323844c86ee65e981b16492134feb
MikeKowalsky <mkowalewski83@gmail.com>
2018-12-29 20:35:34 +0100

    finished relations with functions - both ways

--- app.js
+++ app.js
@@ -13,6 +13,28 @@ const app = express();
 app.use(bodyParser.json());
 
 //function to populate manually
+//-> same as below
+const events = eventIds => {
+  //MongoDB query syntax $in -> search events with id from the list of id
+  return Event.find({ _id: { $in: eventIds } })
+    .then(events => {
+      return events.map(event => {
+        return {
+          ...event._doc,
+          _id: event.id,
+          creator: user.bind(this, event.creator) //thanks to hoisting I can call this user function to "populate" this object
+          //because creator property should not contain single value but instead it call a function, when try to access it
+          //grahQL is checking the property: if it's a value it retunrs the value, if a function than returns the result of that function
+          //but there is no infinite loop like with "automated populate" (traditional mongoose one) because if we don't requesting particulat data from query level the function will not be executed
+        };
+      });
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
+//function to populate manually
 //returning a user object with overwritten _id -> object by String
 //basically I can fetch the user by id
 //"manual populational approach"
@@ -21,7 +43,9 @@ const user = userId => {
     .then(user => {
       return {
         ...user._doc,
-        _id: user.id
+        _id: user.id,
+        //same here, I don't want to have this values, insted I would like to call a function
+        createdEvents: events.bind(this, user._doc.createdEvents)
       };
     })
     .catch(err => {
@@ -104,7 +128,11 @@ app.use(
         return event
           .save()
           .then(result => {
-            createdEvent = { ...result._doc, _id: result.id };
+            createdEvent = {
+              ...result._doc,
+              _id: result.id,
+              creator: user.bind(this, result._doc.creator)
+            };
             return User.findById("5c27a9f5aa7c243a9962458f");
             // console.log(result);
             // return { ...result._doc, _id: result._doc._id.toString() }; //this syntax because result contains a lot of metadata, and this _doc is close to our object
