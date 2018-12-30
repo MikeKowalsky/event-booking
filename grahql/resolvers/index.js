196b8bbc07a1ba3e3d00622482b6c4a3dc81f826
MikeKowalsky <mkowalewski83@gmail.com>
2018-12-30 03:03:41 +0100

    added booking schemas and resolvers

--- grahql/resolvers/index.js
+++ grahql/resolvers/index.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 
 const Event = require("../../models/event");
 const User = require("../../models/user");
+const Booking = require("../../models/booking");
 
 //whole code with promises
 // // function to populate manually
@@ -43,6 +44,20 @@ const events = async eventIds => {
   }
 };
 
+const singleEvent = async eventId => {
+  try {
+    const event = await Event.findById(eventId);
+    return {
+      ...event._doc,
+      _id: event.id,
+      date: new Date(event._doc.date).toISOString(),
+      creator: user.bind(this, event.creator)
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
 //again promises
 // //function to populate manually
 // //returning a user object with overwritten _id -> object by String
@@ -68,6 +83,7 @@ const user = async userId => {
     return {
       ...user._doc,
       _id: user.id,
+      // passowrd: null, // should be here as well
       createdEvents: events.bind(this, user._doc.createdEvents)
     };
   } catch (err) {
@@ -96,13 +112,29 @@ module.exports = {
   events: async () => {
     try {
       const events = await Event.find();
-
       return events.map(event => {
         return {
           ...event._doc,
           _id: event.id,
           date: new Date(event._doc.date).toISOString(),
           creator: user.bind(this, event._doc.creator)
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find();
+      return bookings.map(booking => {
+        return {
+          ...booking._doc,
+          _id: booking.id,
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event),
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString()
         };
       });
     } catch (err) {
@@ -216,5 +248,41 @@ module.exports = {
       console.log(err);
       throw err;
     }
+  },
+  bookEvent: async args => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      user: "5c27dfdd40d6163e5638599b",
+      event: fetchedEvent
+    });
+    const result = await booking.save();
+    return {
+      ...result._doc,
+      _id: result.id,
+      user: user.bind(this, result._doc.user),
+      event: singleEvent.bind(this, result._doc.event),
+      createdAt: new Date(result._doc.createdAt).toISOString(),
+      updatedAt: new Date(result._doc.updatedAt).toISOString()
+    };
+  },
+  cancelBooking: async args => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate("event");
+      const event = {
+        ...booking.event._doc,
+        _id: booking.event.id,
+        creator: user.bind(this, booking.event._doc.creator)
+      };
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
+    } catch (err) {
+      throw err;
+    }
   }
 };
+
+//Setting the below globally fixed the ID issue for me
+// const { ObjectId } = mongoose.Types
+// ObjectId.prototype.valueOf = function () {
+//   return this.toString()
+// }
