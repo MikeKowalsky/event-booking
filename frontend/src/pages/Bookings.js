2896103a4fa98839e739e97cb3c2e8fd4cda6f15
MikeKowalsky <mkowalewski83@gmail.com>
2019-01-30 21:01:39 +0100

    handled bookings

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,8 +1,79 @@
 import React, { Component } from "react";
 
+import Spinner from "../components/Spinner/Spinner";
+
+import UserContext from "../context/user-context";
+
 class BookingsPage extends Component {
+  state = {
+    isLoading: false,
+    bookings: []
+  };
+
+  static contextType = UserContext;
+
+  componentDidMount() {
+    this.fetchBookings();
+  }
+
+  fetchBookings = () => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+          query {
+            bookings {
+              _id
+              createdAt
+              event {
+                _id
+                title
+                date
+              }
+            }
+          }
+        `
+    };
+
+    fetch("http://localhost:4000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.context.token}`
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        const bookings = resData.data.bookings;
+        this.setState({ bookings, isLoading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  };
+
   render() {
-    return <h1>This is bookings page</h1>;
+    return (
+      <React.Fragment>
+        {this.state.isLoading ? (
+          <Spinner />
+        ) : (
+          <ul>
+            {this.state.bookings.map(booking => (
+              <li key={booking._id}>
+                {booking.event.title} - {booking.createdAt}
+              </li>
+            ))}
+          </ul>
+        )}
+      </React.Fragment>
+    );
   }
 }
 
